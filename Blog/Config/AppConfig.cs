bbe49c75e021bebf19755df9f8eeafd19ab03a4a
Eddy Ortega <cofter11@gmail.com>
2024-02-25 12:11:44 -0800

    feat: template

---
+++ Blog/Config/AppConfig.cs
@@ -0,0 +1,16 @@
+﻿namespace Blog.Config
+{
+  public static class AppConfig
+  {
+    public static WebApplication AppConfigPlugin(this WebApplication app)
+    {
+      if (app.Environment.IsDevelopment())
+      {
+        app.UseSwagger();
+        app.UseSwaggerUI();
+      }
+      app.UseHttpsRedirection();
+      return app;
+    }
+  }
+}
---
+++ Blog/Config/ServiceConfig.cs
@@ -0,0 +1,13 @@
+﻿namespace Blog.Config
+{
+  public static class ServiceConfig
+  {
+    public static WebApplicationBuilder ServiceConfigPlugin(this WebApplicationBuilder builder)
+    {
+      builder.Services.AddEndpointsApiExplorer();
+      builder.Services.AddSwaggerGen();
+      return builder;
+    }
+  }
+}
+
